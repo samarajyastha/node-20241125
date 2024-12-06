b936fb5f0017057526a9354f26472924a1ccf800
Samarajya Shrestha <samarajyastha@gmail.com>
2024-12-06 21:45:07 +0545

    feat: implement hbs template

--- src/app.js
+++ src/app.js
@@ -6,12 +6,26 @@ import dotenv from "dotenv";
 import logger from "./middlewares/logger.js";
 import authRoute from "./routes/auth.js";
 import productsRoute from "./routes/products.js";
+import authMiddleware from "./middlewares/auth.js";
+import productService from "./services/productService.js";
+import url from "url";
+import path from "path";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 dotenv.config();
 
 connectDB();
+
+app.set("view engine", "hbs");
+
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "../public/assets/images"))
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -66,6 +80,32 @@ app.use("/api/products", productsRoute);
 
 //   res.json(user);
 // });
+
+app.get("/login", (req, res) => {
+  res.render("login", { user: "John" });
+});
+
+app.get("/home", authMiddleware, async (req, res) => {
+  const user = req.user;
+
+  const products = await productService.getAllProducts({
+    limit: 10,
+    offset: 0,
+  });
+
+  res.render("index", { user, products });
+});
+
+app.get("/products/:id", authMiddleware, async (req, res) => {
+  const user = req.user;
+  const id = req.params.id;
+
+  console.log(id);
+
+  const product = await productService.getProductById(id);
+
+  res.render("product", { user, product });
+});
 
 const PORT = process.env.PORT;
 
